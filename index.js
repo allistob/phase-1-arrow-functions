de0d9c6d8dddec8eec89a5c534cc0c9bc548fbc5
Allison Tobar <allisontobarrodriguez@gmail.com>
2023-04-20 17:58:32 -0400

    completed lab

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = () => {
+return 2000/ 100;
+}
+
+const square = (parameter1) => {
+    return parameter1 * parameter1;
+}
+
+const add = (parameter1, parameter2) => {
+    return parameter1 + parameter2;
+}
